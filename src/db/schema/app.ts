58cdd5a1df51cf4c6de9c762784ae0efd9f7edde
Really Cortez <cortezreally0@gmail.com>
2026-01-27 20:13:54 +0800

    implement coderabbit suggested

--- src/db/schema/app.ts
+++ src/db/schema/app.ts
@@ -37,5 +37,5 @@ export const subjectsRelations = relations(subjects,
 export type Department = typeof departments.$inferSelect;
 export type NewDepartment = typeof departments.$inferInsert;
 
-export type subject = typeof departments.$inferSelect;
-export type NewSubject = typeof departments.$inferInsert;
+export type Subject = typeof subjects.$inferSelect;
+export type NewSubject = typeof subjects.$inferInsert;
