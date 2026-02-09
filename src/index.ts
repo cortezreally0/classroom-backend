e8bf37b0635ea61a5fee815b2363c62424288cf7
Really Cortez <cortezreally0@gmail.com>
2026-02-09 19:06:56 +0800

    adding .js extension

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import usersRouter from "./routes/users.js";
 import classesRouter from "./routes/classes.js";
 import securityMiddleware from "./middleware/security.js";
 import {toNodeHandler} from "better-auth/node";
-// import {auth} from "./lib/auth";
+// import {auth} from "./lib/auth.js";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
